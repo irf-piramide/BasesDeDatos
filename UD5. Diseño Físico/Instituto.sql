ac581400464276eca73ef2a1e2e61e034c49b4f7
ivan <230085@fppiramide.com>
2024-01-17 16:20:53 +0100

    finalizado

--- UD5. Diseño Físico/Instituto.sql
+++ UD5. Diseño Físico/Instituto.sql
@@ -14,7 +14,7 @@ CONSTRAINT pk_Ciclo PRIMARY KEY(codigo)
 CREATE TABLE Modulo (
 nombre varchar (50),
 horas int (3) NOT NULL,
-CONSTRAINT pk_Modulo PRIMARY KEY(Nombre)
+CONSTRAINT pk_Modulo PRIMARY KEY(nombre)
 );
 
 CREATE TABLE Docente (
@@ -31,10 +31,23 @@ codigoDepartamento int (3) NOT NULL,
 codigoCiclo int (3) NOT NULL,
 CONSTRAINT pk_id PRIMARY KEY(id),
 CONSTRAINT fk_Departamento_Ciclo_Departamento FOREIGN KEY (codigoDeparmento) REFERENCES Departamento (codigo) ON DELETE CASCADE,
-CONSTRAINT fk_Departamento_Ciclo_Ciclo FOREIGN KEY (codigoCiclo) REFERENCES Ciclo (codigo) ON DELETE CASCADE,
+CONSTRAINT fk_Departamento_Ciclo_Ciclo FOREIGN KEY (codigoCiclo) REFERENCES Ciclo (codigo) ON DELETE CASCADE
 );
 
 CREATE TABLE Ciclo_Modulo (
 id int (9) AUTO_INCREMENT,
-codigoCiclo int (3)
+codigoCiclo int (3),
+nombreModulo varchar (50),
+CONSTRAINT pk_id PRIMARY KEY (id),
+CONSTRAINT fk_Ciclo_Modulo_Ciclo FOREIGN KEY (codigoCiclo) REFERENCES Ciclo (codigo) ON DELETE CASCADE,
+CONSTRAINT Fk_Ciclo_Modulo_Modulo FOREIGN KEY (nombreModulo) REFERENCES Modulo (nombre) ON DELETE CASCADE
+);
+
+CREATE TABLE Modulo_Docente (
+id int (9) AUTO_INCREMENT,
+nombreModulo varchar (50),
+dniDocente int (8),
+CONSTRAINT pk_id PRIMARY KEY (id),
+CONSTRAINT fk_Modulo_Docente_Modulo FOREIGN KEY (nombreModulo) REFERENCES Modulo (nombre) ON DELETE CASCADE,
+CONSTRAINT fk_Modulo_Docente_Docente FOREIGN KEY (dniDocente) REFERENCES Docente (dni) ON DELETE CASCADE
 );
